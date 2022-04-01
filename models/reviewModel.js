4bbd337929856caa916864fbb02113ec5060ee2f
Sanjay Khatri <sanzaikc@gmail.com>
2022-04-01 09:13:09 +0545

    mend

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -37,10 +37,10 @@ const reviewSchema = new mongoose.Schema(
 reviewSchema.pre(/^find/, async function (next) {
   this.populate({
     path: 'tour',
+    select: 'name',
   }).populate({
     path: 'user',
-    select:
-      '-__v -passwordChangedAt -passwordResetExpiresIn -passwordResetToken',
+    select: 'name photo',
   });
 
   next();
