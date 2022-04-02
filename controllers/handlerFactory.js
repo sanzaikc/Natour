8417190b7ab91652200b71d595b4b0e67490a405
Sanjay Khatri <sanzaikc@gmail.com>
2022-04-02 10:54:01 +0545

    updateOne document factory handler implemented

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -1,6 +1,24 @@
 const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
 
+exports.updateOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!doc)
+      return next(new AppError("Couldn't find document with that ID", 404));
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        data: doc,
+      },
+    });
+  });
+
 exports.deleteOne = (Model) =>
   catchAsync(async (req, res, next) => {
     const doc = await Model.findByIdAndDelete(req.params.id);
@@ -13,15 +31,3 @@ exports.deleteOne = (Model) =>
       data: null,
     });
   });
-
-// exports.deleteTour = catchAsync(async (req, res, next) => {
-//   const tour = await Tour.findByIdAndDelete(req.params.id);
-
-//   if (!tour)
-//     return next(new AppError("Couldn't find tour with that 'id'", 404));
-
-//   res.status(204).json({
-//     status: 'success',
-//     message: 'Tour deleted',
-//   });
-// });
--- controllers/userController.js
+++ controllers/userController.js
@@ -66,11 +66,6 @@ exports.getUser = (req, res) => {
   });
 };
 
-exports.updateUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'api under development',
-  });
-};
+exports.updateUser = factory.updateOne(User);
 
 exports.deleteUser = factory.deleteOne(User);
