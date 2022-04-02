7debe0677e82f5e0ca11b1fc6d6abf7a4874fbb6
Sanjay Khatri <sanzaikc@gmail.com>
2022-04-02 08:53:19 +0545

    mend

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -3,7 +3,8 @@ const express = require('express');
 const authController = require('./../controllers/authController');
 const reviewController = require('./../controllers/reviewController');
 
-const router = express.Router({ mergeParams: true }); //Merging params from tour router
+//Merging params from tour router
+const router = express.Router({ mergeParams: true });
 
 router
   .route('/')
